bbadd8a1d37b1a45cfb30746abe6b0602063fafc
matthew-giannola <78050003+matthew-giannola@users.noreply.github.com>
2021-10-05 20:35:58 -0500

    Code

--- Project 1 Data com/module1.py
+++ Project 1 Data com/module1.py
@@ -3,9 +3,10 @@ from threading import Thread
 from socket import *
 import sys  # In order to terminate the program
 
+serverAddress = '192.168.0.107'
 serverSocket = socket(AF_INET, SOCK_STREAM) # Alternative (better) syntax
 serverPort = 6789
-serverSocket.bind(('192.168.0.107', serverPort))
+serverSocket.bind((serverAddress, serverPort))
 serverSocket.listen(5)
 
 # Server should be up and running and listening to the incoming connections
